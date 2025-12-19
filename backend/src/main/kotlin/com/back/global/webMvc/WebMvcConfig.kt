bb9f7374fe52c17ae9720cfb2fe6921886f04c20
jhs512 <jangka512@gmail.com>
2025-12-19 09:56:36 +0900

    work

--- backend/src/main/kotlin/com/back/global/webMvc/WebMvcConfig.kt
+++ backend/src/main/kotlin/com/back/global/webMvc/WebMvcConfig.kt
@@ -4,11 +4,14 @@ import com.back.global.app.AppConfig.Companion.getGenFileDirPath
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.io.File
 
 @Configuration
 class WebMvcConfig : WebMvcConfigurer {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        val absolutePath = File(getGenFileDirPath()).absolutePath
+
         registry.addResourceHandler("/gen/**")
-            .addResourceLocations("file:///" + getGenFileDirPath() + "/")
+            .addResourceLocations("file:///$absolutePath/")
     }
 }
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,37 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        // /post/ 로 시작하는 모든 경로를
+        source: '/post/:path*',
+
+        // /p/ 로 영구 이동
+        destination: '/p/:path*',
+
+        // true면 308(사실상 301급)로 고정 리다이렉트
+        permanent: true,
+      },
+
+      // (선택) /post 자체도 /p로
+      {
+        source: '/post',
+        destination: '/p',
+        permanent: true,
+      },
+    ];
+  },
   images: {
     dangerouslyAllowSVG: true,
+    unoptimized: process.env.NODE_ENV === "development",
     remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8080",
+        pathname: "/**",
+      },
       {
         protocol: "http",
         hostname: "*.kakaocdn.net",
